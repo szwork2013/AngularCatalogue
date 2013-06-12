6f42d35bf9d053e819799a347be14db129352e37
Colin Mackay <colin.mackay@xedosoftware.com>
2013-06-12 10:50:00 +0100

    Wire up initial page
    Wire up the initial page with Angular JS, Twitter Bootstrap and a basic
    MVC 4 view

---
+++ src/AngularCatalogue.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Optimization;
+
+namespace AngularCatalogue.Web
+{
+    public class BundleConfig
+    {
+         public static void RegisterBundles(BundleCollection bundles)
+         {
+             bundles.Add(new ScriptBundle("~/Scripts/base-frameworks.js")
+                 .Include("~/Scripts/jquery-{version}.js")
+                 .Include("~/Scripts/angular.js")
+                 .Include("~/Scripts/bootstrap.js"));
+
+             bundles.Add(new StyleBundle("~/Content/base-styles.css")
+                 .Include("~/Content/bootstrap.css"));
+         }
+    }
+}
