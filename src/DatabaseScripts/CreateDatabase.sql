64c3edffd8df825a293494179687807a18a97b5f
Colin Mackay <hello@xander-framework.org>
2013-06-12 22:52:08 +0100

    Add more products to the database
    Add more products to the database

--- src/DatabaseScripts/CreateDatabase.sql
+++ src/DatabaseScripts/CreateDatabase.sql
@@ -198,7 +198,7 @@ VALUES ('Addidas'), ('American Apparel'), ('Andrew Christian'), ('ASOS'),
   ('Speedo'), ('Ted Baker'), ('United Colors of Benetton'), ('Vans')
   
 INSERT INTO ProductTypes(Caption)
-VALUES ('T-Shirt'), ('Jeans')
+VALUES ('T-Shirt'), ('Jeans'), ('Polo-Shirt')
   
 INSERT INTO Styles(Caption)
 VALUES ('Long sleeve'), ('Regular sleeve'), ('Rolled Sleeve'), ('Capped Sleeve'),
@@ -212,7 +212,15 @@ VALUES
 	(4,1,4,'Eksell', 'tshirt-4.png'), -- ASOS, tshirt,capped-sleeve
 	(4,2,5,'Jim''s', 'jeans-5.png'), -- ASOS, Jeans, slim-fit
 	(4,2,5,'Bob''s', 'jeans-6.png'), -- ASOS, Jeans, regular-fit
-	(14,2,6,'501s', 'jeans-7.png') -- Levis, Jeans, boot-cut
+	(14,2,6,'501s', 'jeans-7.png'), -- Levis, Jeans, boot-cut
+	(13,3,5,'Planet', 'polo-shirt-8.png'),
+	(13,3,5,'Mercury', 'polo-shirt-9.png'),
+	(13,3,5,'Venus', 'polo-shirt-10.png'),
+	(13,3,5,'Mars', 'polo-shirt-11.png'),
+	(13,3,5,'Saturn', 'polo-shirt-12.png'),
+	(13,3,5,'Jupiter', 'polo-shirt-13.png'),
+	(13,3,5,'Neptune', 'polo-shirt-14.png')
+	
 	
 INSERT INTO ProductVariants(ProductId, SizeId, ColourId, InventoryCount)
 VALUES
@@ -228,8 +236,16 @@ VALUES
 	(6,19,6,6),(6,20,6,5),(6,21,6,5),(6,22,6,5),(6,23,6,5),(6,24,6,5),
 	(7,7,6,1),(7,8,6,5),(7,9,6,5),(7,10,6,5),(7,11,6,5),(7,12,6,5),
 	(7,13,6,2),(7,14,6,5),(7,15,6,5),(7,16,6,5),(7,17,6,5),(7,18,6,5),
-	(7,19,6,3),(7,20,6,5),(7,21,6,5),(7,22,6,5),(7,23,6,5),(7,24,6,5)
-	
+	(7,19,6,3),(7,20,6,5),(7,21,6,5),(7,22,6,5),(7,23,6,5),(7,24,6,5),
+	(8,1,8,1),(8,2,8,1),(8,3,8,1),(8,4,8,1),(8,5,8,1),(8,6,8,1),
+	(9,1,2,1),(9,2,2,1),(9,3,2,1),(9,4,2,1),(9,5,2,1),(8,6,2,1),
+	(10,1,7,1),(10,2,7,1),(10,3,7,1),(10,4,7,1),(8,5,7,1),(8,6,7,1),
+	(10,1,15,1),(10,2,15,1),(10,3,15,1),(10,4,15,1),(8,5,15,1),(8,6,15,0),
+	(11,1,10,1),(11,2,10,1),(11,3,10,1),(11,4,10,1),(11,5,10,1),(11,6,10,1),
+	(12,1,10,1),(12,2,10,1),(12,3,10,1),(12,4,10,1),(12,5,10,1),(12,6,10,1),
+	(13,1,14,1),(13,2,14,1),(13,3,14,1),(13,4,14,1),(13,5,14,1),(13,6,14,1),
+	(14,1,8,1),(14,2,8,1),(14,3,8,1),(14,4,8,1),(14,5,8,1),(14,6,8,1)
+
 
 SELECT * FROM FlattenedProducts
 SELECT * FROM FlattenedProductVariants
